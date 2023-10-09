47c5342a8d7026eb92399c6e77ce241137bf302d
tofu345 <shammahayotunde1@gmail.com>
2023-10-09 18:48:21 +0100

    next question

--- src/main.rs
+++ src/main.rs
@@ -47,9 +47,7 @@ fn handle(mut stream: TcpStream, _addr: SocketAddr) {
                 let contents = fs::read_to_string(file_path.clone());
 
                 if req.method == "POST" {
-                    println!("body: {}", req.body);
                     fs::write(file_path, req.body).expect("unable to write");
-
                     break 'files (201, None);
                 }
 

d3a17a724767bb43f1df473cafc25aee0d98a7f0
tofu345 <shammahayotunde1@gmail.com>
2023-10-09 10:18:15 +0100

    200 on / else 400

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,29 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let _data = stream.read(&mut [0; 1024]).unwrap();
-                stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+                let mut data = [0; 1024];
+                let _data = stream.read(&mut data).unwrap();
+
+                let data: Vec<u8> = data.into_iter().collect();
+                let data = match String::from_utf8(data) {
+                    Ok(v) => v,
+                    Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+                };
+
+                let first_line = match data.split("\r\n").next() {
+                    Some(v) => v,
+                    None => panic!("Invalid HTTP request"),
+                };
+
+                let path = first_line.split(" ").collect::<Vec<&str>>()[1];
+
+                if path == "/" {
+                    stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+                } else {
+                    stream
+                        .write("HTTP/1.1 404 NOT FOUND\r\n\r\n".as_bytes())
+                        .unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
